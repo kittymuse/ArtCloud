5319cd932098c5b2069aa836f112ca689586dd02
冬字差一人 <329636491@qq.com>
2020-01-15 15:46:44 +0800

    创建"我的"相关migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->smallInteger('studio_id')->nullable()->comment('机构ID');
+            $table->string('phone', 11)->unique()->comment('手机号');
+            $table->string('name', 32)->comment('昵称');
+            $table->string('avatar', 100)->comment('头像');
+            $table->tinyInteger('sex')->nullable()->comment('性别');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_01_15_153639_create_campuses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->smallInteger('studio_id')->comment('机构ID');
+            $table->string('name', 32)->comment('名称');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campuses');
+    }
+}
