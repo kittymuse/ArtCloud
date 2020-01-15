002f06c6f6371a27073d76f1ac9afc21c8290e0b
冬字差一人 <329636491@qq.com>
2020-01-15 16:02:33 +0800

    移动User到Models下

--- app/Http/Models/User.php
+++ app/Http/Models/User.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -16,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'studio_id', 'phone', 'name', 'avatar', 'sex'
     ];
 
     /**
@@ -24,16 +23,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = [];
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
 }
