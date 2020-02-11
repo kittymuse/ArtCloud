e348b7617cb94550f2e7daadfc0c140c88fc6364
冬字差一人 <329636491@qq.com>
2020-02-11 21:58:32 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,13 @@ use Illuminate\Http\Request;
 */
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
 	Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function(){
-		//短信验证码
+		// 短信验证码
 		Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
+		// 登录
+    	Route::post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
+    	// 刷新Token
+	    Route::put('authorizations/current', 'AuthorizationsController@update')->name('authorizations.update');
+	    // 删除Token
+	    Route::delete('authorizations/current', 'AuthorizationsController@destroy')->name('authorizations.destroy');
 	});
 });
