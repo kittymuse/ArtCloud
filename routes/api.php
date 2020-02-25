88aabb5530578f0e200b79f559c7db3a7f6e6fcc
冬字差一人 <329636491@qq.com>
2020-02-25 23:18:43 +0800

    删除接口的name

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Http\Request;
 Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
 	Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function () {
 		// 短信验证码
-		Route::post('verificationCodes', 'VerificationCodeController@send')->name('verificationCodes.send');
+		Route::post('verificationCodes', 'VerificationCodeController@send');
         // 登录
-        Route::post('authorizations', 'AuthorizationController@login')->name('authorizations.login');
+        Route::post('authorizations', 'AuthorizationController@login');
 	});
 
 	Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
@@ -26,19 +26,19 @@ Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
         // 登录后可以访问的接口
         Route::middleware('api.refresh')->group(function() {
             // 我的信息
-            Route::get('user', 'UserController@info')->name('user.info');
+            Route::get('user', 'UserController@info');
             // 修改我的信息
-            Route::patch('user', 'UserController@update')->name('user.update');
+            Route::patch('user', 'UserController@update');
             // 上传图片
-            Route::post('images', 'ImageController@store')->name('images.store');
+            Route::post('images', 'ImageController@store');
             // 新建收货地址
-            Route::post('user/addresses', 'UserAddressesController@store')->name('user.addresses.store');
+            Route::post('user/addresses', 'UserAddressesController@store');
             // 修改收货地址
-            Route::patch('user/addresses', 'UserAddressesController@update')->name('user.addresses.update');
+            Route::patch('user/addresses', 'UserAddressesController@update');
             // 删除收货地址
-            Route::delete('user/addresses', 'UserAddressesController@destroy')->name('user.addresses.destroy');
+            Route::delete('user/addresses', 'UserAddressesController@destroy');
             // 退出
-            Route::delete('authorizations', 'AuthorizationController@logout')->name('authorizations.logout');
+            Route::delete('authorizations', 'AuthorizationController@logout');
         });
 	});
 });
